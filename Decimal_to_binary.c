7e7988172fe27aa0fe192381ff5c8415d9715e01
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-03-09 19:57:44 +0530

    Converet decimal number to binary numbers

--- Decimal_to_binary.c
+++ Decimal_to_binary.c
@@ -1,7 +1,27 @@
-#include<stdio.h>
-int binary(int);
-int main()
-{
-    int n;
-    printf("Enter number to get binary value:");
+#include <stdio.h>
+
+void decToBinary(int decimal);
+
+int main() {
+    int decimal;
+    
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal);
+    
+    printf("Binary equivalent of %d is: ", decimal);
+    decToBinary(decimal);
+    
+    return 0;
+}
+
+void decToBinary(int decimal) {
+    int remainder = decimal % 2;
+    
+    if (decimal <= 1) {
+        printf("%d", decimal);
+        return;
+    }
+    
+    decToBinary(decimal / 2);
+    printf("%d", remainder);
 }
