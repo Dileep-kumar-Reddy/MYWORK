388d426c3ba5aa880ca343e9a6743debff164afe
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-03-13 19:41:09 +0530

    Small Code

--- Python/Beta.py
+++ Python/Beta.py
@@ -1,5 +1,12 @@
-import sys
-print("python version")
-print(sys.version)
-print("Version info")
-print(sys.version_info)
+# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space #between them
+first_name=input("Enter first name:")
+last_name=input("Enter last name:")
+con=first_name+last_name
+print(len(con))
+for i in range(len(con)-1,-1,-1):
+    if(i==len(last_name)):
+        print(" ",end="")
+    print(con[i],end="")
+
+
+
