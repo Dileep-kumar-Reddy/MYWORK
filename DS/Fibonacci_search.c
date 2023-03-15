c50925c2e48367762a6a5859c538077b808ef86f
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-03-15 19:05:50 +0530

    Fibanacci search

---
+++ DS/Fibonacci_search.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int size,a[size],fib[size],i;
+    printf("Enter size of array:");
+    scanf("%d",&size);
+    fib[0]=0,fib[1]=1;
+    for(i=2;i<size;i++)
+    {
+        
+    }
+}
