c26f29eac7f10a121e13972fadaf442729908262
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-06-11 21:57:22 +0530

    Bug should be fixed

--- DS/Binary_search.c
+++ DS/Binary_search.c
@@ -1,45 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int size,i,beg=0,end=size-1,key,pos=-1;
+    int size, i, beg = 0, end = size - 1, key, pos = -1;
     printf("Enter the size of array:");
-    scanf("%d",&size);
+    scanf("%d", &size);
     int a[size];
     printf("Enter the elements in array(Ascending order):");
-    for(i=0;i<size;i++)
-    scanf("%d",&a[i]);
-    i=1;
+    for (i = 0; i < size; i++)
+        scanf("%d", &a[i]);
+    i = 1;
     printf("Enter an element to search:");
-    scanf("%d",&key);
-    while(beg!=size)
+    scanf("%d", &key);
+    while (beg <= size)
     {
-        int mid=(beg+end)/2;
-        if(a[mid]==key)
+        int mid = (beg + end) / 2;
+        if (a[mid] == key)
         {
-            pos=mid;
-            printf("Element is found at %d\n",pos+1);
-            while(a[pos+i]==a[pos]||a[pos-i]==a[pos])
+            pos = mid;
+            printf("Element is found at %d\n", pos + 1);
+            while (a[pos + i] == a[pos] || a[pos - i] == a[pos])
             {
-                printf("Element is also found at %d",pos+i+1);
+                printf("Element is also found at %d", pos + i + 1);
                 i++;
             }
-            break;
+            return 0;
         }
         else
         {
-            if(key<a[mid])
+            if (key < a[mid])
             {
-                beg=0;
-                end=mid-1;
+                beg = 0;
+                end = mid - 1;
             }
             else
             {
-                beg=mid+1;
-                end=size-1;
+                beg = mid + 1;
+                end = size - 1;
             }
         }
-        
     }
-    if(pos==-1)
-    printf("Element not found");
+    if (pos == -1)
+        printf("Element not found");
 }
