327d6b5f917be13c8ea1f4510efe18751d37ef86
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-06-24 22:04:51 +0530

    Bubble sorting of elements

--- DS/Bubble_sort.c
+++ DS/Bubble_sort.c
@@ -1,32 +1,32 @@
-#include<stdio.h>
-int swap(int [],int,int);
+#include <stdio.h>
+int swap(int[], int, int);
 int main()
 {
-    int i,size,j;
+    int i, size, j;
     printf("Enter the size of array:");
-    scanf("%d",&size);
+    scanf("%d", &size);
     int a[size];
-    printf("Enter %d elements in tha array:\n",size);
-    for(i=0;i<size;i++)
-    scanf("%d",&a[i]);
-    for(i=0;i<size;i++)
+    printf("Enter %d elements in tha array:\n", size);
+    for (i = 0; i < size; i++)
+        scanf("%d", &a[i]);
+    for (i = 0; i < size; i++)
     {
-        for(j=0;j<size-i-1;j++)
+        for (j = 0; j < size - i - 1; j++)
         {
-            if(a[j]>a[j+1])
+            if (a[j] > a[j + 1])
             {
-                swap(a,j,j+1);
+                swap(a, j, j + 1);
             }
         }
     }
     printf("Bubble sorted array is:\n");
-    for(i=0;i<size;i++)
-    printf("%3d",a[i]);
+    for (i = 0; i < size; i++)
+        printf("%3d", a[i]);
 }
-int swap(int a[],int i,int j)
+int swap(int a[], int i, int j)
 {
     int swap;
-    swap=a[j];
-    a[j]=a[i];
-    a[i]=swap;
+    swap = a[j];
+    a[j] = a[i];
+    a[i] = swap;
 }
