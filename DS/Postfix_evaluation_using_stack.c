af59ef33ddc17c52771b24ce8455e3fd79ac5a2c
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-06-25 11:10:56 +0530

    Code to evaluate postfix expression using stack

---
+++ DS/Postfix_evaluation_using_stack.c
@@ -0,0 +1,46 @@
+// Need to check
+#include <stdio.h>
+#include <ctype.h>
+int stack[20];
+int stack_top = 0;
+int push(int num)
+{
+    stack[++stack_top] = num;
+}
+int pop()
+{
+    return stack[stack_top--];
+}
+int main()
+{
+    char exp[20];
+    int i = 0, opr_1, opr_2;
+    printf("Enter a postfix expression:");
+    scanf("%s", exp);
+    while (exp[i] != '\0')
+    {
+        exp[i] = exp[i] - 48;
+        if (isdigit(exp[i]))
+        {
+            push(exp[i]);
+        }
+        else
+        {
+            opr_1 = pop();
+            opr_2 = pop();
+            switch (exp[i])
+            {
+            case '+':
+                push(opr_1 + opr_2);
+            case '-':
+                push(opr_1 - opr_2);
+            case '*':
+                push(opr_1 * opr_2);
+            case '/':
+                push(opr_1 / opr_2);
+            }
+        }
+    }
+    i++;
+    printf("%d", pop());
+}
