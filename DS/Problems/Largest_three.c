87c6defa72157859445c6c8b7f3febff36af1e1e
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-07-29 10:53:47 +0530

    Last three elements of sorted array

---
+++ DS/Problems/Largest_three.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, j;
+    printf("Enter no.of elements:");
+    scanf("%d", &n);
+    int a[n];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    printf("%d %d %d", a[n - 1], a[n - 2], a[n - 3]);
+}
