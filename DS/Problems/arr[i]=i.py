3e84a6a1ac5d5984aa83be3460f2e85c14d12cba
Dileep-kumar-Reddy <126312880+Dileep-kumar-Reddy@users.noreply.github.com>
2023-08-05 09:44:49 +0530

    added changes

--- DS/Problems/arr[i]=i.py
+++ DS/Problems/arr[i]=i.py
@@ -1,13 +1,15 @@
+# sort an array in the form of 1,2---,n if the element is not present -1 will be placed
 import numpy as np
 
+# zero should be given as input
 print("Enter elements:", end="")
-a = np.array(input().split(","), dtype="int16")
+a = np.array(input().split(","), dtype="int32")
 x = np.max(a)
-b = np.zeros(x, dtype="int16")
+b = np.zeros(x, dtype="int")
 for i in range(x):
-    if b[i] != i:
-        if i in a:
-            b[i] = i
+    if b[i] != i + 1:
+        if i + 1 in a:
+            b[i] = i + 1
         else:
             b[i] = -1
 print(b)
