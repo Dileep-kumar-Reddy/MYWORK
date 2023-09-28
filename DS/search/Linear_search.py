c3ed197a973613d489dc8a63f11c4cd4f2dd000c
Dileep Reddy <challa.dileepkumarreddy@gmail.com>
2023-09-28 15:30:36 +0530

    Changed to python

--- DS/search/Linear_search.py
+++ DS/search/Linear_search.py
@@ -1,20 +1,14 @@
-import time
-
-
 class LinearSearch:
     def __init__(self: object, arr: list, key: int) -> None:
         self.array = arr
         self.search_element = key
 
     def Searching(self) -> int:
-        for i in self.array:
-            if self.search_element == i:
+        for i in range(len(self.array)):
+            if self.search_element == self.array[i]:
                 return i
         return -1
 
 
-a = LinearSearch([10, 42, 5, 1, 4, 3], 4)
-start_time = time.time()
+a = LinearSearch([10, 42, 5, 1, 4, 3], 3)
 print(a.Searching())
-end_time = time.time()
-print(end_time - start_time)
