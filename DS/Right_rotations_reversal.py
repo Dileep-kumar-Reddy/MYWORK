637f8b1eee068888d2706c4d272d727f6c8f57df
Dileep Reddy <challa.dileepkumarreddy@gmail.com>
2023-10-10 20:10:12 +0530

    Right Rotations by reversal

--- DS/Right_rotations_reversal.py
+++ DS/Right_rotations_reversal.py
@@ -1,19 +1,13 @@
-def reverse(start: int, stop: int, arr: list) -> None:
+def reverse(start: int, stop: int, arr: list) -> list:
     while start < stop:
         arr[start], arr[stop] = arr[stop], arr[start]
         start += 1
         stop -= 1
+    return arr
 
 
-def rotate_array(array: list, rotations: int) -> None:
-    reverse(-1, -(rotations), array)
-    reverse(-rotations-1, 0, array)
-    reverse(0, len(array) - 1, array)
-
-
-array = list(map(int, input("Enter array: ").split()))
-rotations = int(input("Enter no. of rotations: "))
-
-rotate_array(array, rotations)
-
-print("Rotated array:", array)
+array = list(map(int, input("Enter the array:").split()))
+d = int(input("Enter the rotations:"))
+reverse(-d, -1, array)
+reverse(0, -d-1, array)
+print(reverse(0, len(array)-1, array))
