6a95b4d9dbdc6d27720c890a959dee925414e7ac
Dileep Reddy <challa.dileepkumarreddy@gmail.com>
2023-10-10 20:02:42 +0530

    While rotaing there was a bug now it was fixed

--- DS/Left_rotations_reversal.py
+++ DS/Left_rotations_reversal.py
@@ -1,22 +1,13 @@
-def reverse(start: int, stop: int, arr: list) -> None:
+def reverse(start: int, stop: int, arr: list) -> list:
     while start < stop:
         arr[start], arr[stop] = arr[stop], arr[start]
         start += 1
         stop -= 1
+    return arr
 
 
-def rotate_array(array: list, rotations: int) -> None:
-    # Reverse the first part up to the number of rotations
-    reverse(0, rotations - 1, array)
-    # Reverse the remaining part of the array
-    reverse(rotations, len(array) - 1, array)
-    # Reverse the entire array
-    reverse(0, len(array) - 1, array)
-
-
-array = list(map(int, input("Enter array: ").split()))
-rotations = int(input("Enter no. of rotations: "))
-
-rotate_array(array, rotations)
-
-print("Rotated array:", array)
+array = list(map(int, input("Enter the array:").split()))
+d = int(input("Enter the rotations:"))
+reverse(0, d-1, array)
+reverse(d, len(array)-1, array)
+print(reverse(0, len(array)-1, array))
