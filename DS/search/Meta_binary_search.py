f4fd7368ca1cea66c72ea9b405493501cc07fbce
Dileep Reddy <challa.dileepkumarreddy@gmail.com>
2023-10-12 22:46:39 +0530

    Previously used log2 method in math for length of binary string but it is failing in some cases now fixed that

--- DS/search/Meta_binary_search.py
+++ DS/search/Meta_binary_search.py
@@ -3,16 +3,16 @@ import math
 
 def MetaBinarySearch(arr: list, key: int) -> int:
     n = len(arr)
-    lg = int(math.log2(n - 1)) + 1
+    lg = len(bin(n)[2:])
     pos = 0
     for i in range(lg - 1, -1, -1):
         if arr[pos] == key:
             return pos + 1
         new_pos = pos | 1 << i
         if new_pos < n and arr[new_pos] <= key:
-            pos=new_pos
-        
-    return pos+1 if arr[pos]==key else -1
+            pos = new_pos
+
+    return pos + 1 if arr[pos] == key else -1
 
 
 array = list(map(int, input("Enter the array:").split()))
